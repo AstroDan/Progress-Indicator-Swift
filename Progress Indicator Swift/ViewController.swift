824efc88d9a01cceae2d9ef7d6a0384ffc43ac9c
Dan <Dan@macbook-pro.home>
2014-08-23 13:32:31 +0100

    Initial Commit

---
+++ Progress Indicator Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Progress Indicator Swift
+//
+//  Created by Dan on 23/08/2014.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
